ae53350246b2544054c1571c5eec71c2fac29b8b
marant15 <pensu15@gmail.com>
2017-05-08 08:12:05 -0400

    Add files via upload
    proyecto

---
+++ django_psql/web_final/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^registrop/$', views.product_register, name='logout'),
+    url(r'^search/$', views.search,name='search'),
+]
